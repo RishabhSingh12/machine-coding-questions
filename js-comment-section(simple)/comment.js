d768b4d8b31e9f49dd7966e926bd7248cdaae632
Rishabh Kumar <rishabh2807s@gmail.com>
2023-08-08 16:38:16 +0530

    Added functionality of delete and edit options

--- js-comment-section(simple)/comment.js
+++ js-comment-section(simple)/comment.js
@@ -1,12 +1,16 @@
 let commentContainer = document.getElementById("comment-container");
+let editComment = false;
 
-function createInputBox() {
+function createInputBox(value = "") {
   let div = document.createElement("div");
   div.setAttribute("class", "comment-details");
 
-  div.innerHTML += `<input type="text" placeholder="Add text here" class="input">
-            <button class="btn submit">Submit</button>`;
+  div.innerHTML += `<input type="text" placeholder="Add text here" class="input" value=${value}>
+            <button class="btn submit">${
+              editComment ? "Save" : "Submit"
+            }</button>`;
 
+  editComment = false;
   return div;
 }
 
@@ -17,7 +21,12 @@ function addReply(text) {
   div.innerHTML += `
         <div class="card">
                     <span class="text">${text}</span>
-                    <span class="reply" id="reply">Add reply</span>
+                    <div class="edit-reply-container">
+                        <span class="reply" id="reply">Add reply</span>
+                        <span class="edit" id="edit">Edit</span>
+                        <span class="delete" id="delete">Delete</span>  
+                    </div>
+                    
                 </div>
     `;
 
@@ -26,13 +35,33 @@ function addReply(text) {
 
 commentContainer.addEventListener("click", function (e) {
   let replyClicked = e.target.classList.contains("reply");
+  let editClicked = e.target.classList.contains("edit");
+  let deleteClicked = e.target.classList.contains("delete");
+
   let submitClicked = e.target.classList.contains("submit");
   let closestCard = e.target.closest(".all-comment");
 
+  //clicking on reply button
   if (replyClicked) {
     closestCard.appendChild(createInputBox());
   }
 
+  //clicking on edit button
+  if (editClicked) {
+    let textValue = closestCard.children[0].children[0].textContent;
+    // console.log(textValue);
+
+    editComment = true;
+    closestCard.children[0].remove();
+    closestCard.appendChild(createInputBox(textValue));
+  }
+
+  // deleting comment
+  if (deleteClicked) {
+    closestCard.remove();
+  }
+
+  //clicking on submit btn to add a comment
   if (submitClicked) {
     let commentDetails = e.target.closest(".comment-details");
     if (commentDetails.children[0].value) {
