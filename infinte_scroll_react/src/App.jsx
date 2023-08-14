aa6c3a322255261dbd71b5bf8878fb05cefeb50e
Rishabh Kumar <rishabh2807s@gmail.com>
2023-08-15 01:22:45 +0530

    Added initial files and logic for optimized search query

--- infinte_scroll_react/src/App.jsx
+++ infinte_scroll_react/src/App.jsx
@@ -1,14 +1,37 @@
+import { useEffect } from "react";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [query, setQuery] = useState("");
+  const [response, setResponse] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const searchHandler = (e) => {
+    setQuery(e.target.value);
+  };
+
+  //for calling api
+  const getSearchItems = async () => {};
+
+  useEffect(() => {
+    setLoading(true);
+    getSearchItems();
+  }, []);
 
   return (
     <>
-      <div>Hello</div>
+      <main className="main-container">
+        <h1 className="title">Optimized Search Query</h1>
+        <input
+          value={query}
+          type="search"
+          className="search-input"
+          placeholder="Enter text"
+          onChange={searchHandler}
+        />
+      </main>
     </>
   );
 }
