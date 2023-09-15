9621c85309bf052883c95e5d9c548ffcfdf6607d
Rishabh Kumar <rishabh2807s@gmail.com>
2023-09-15 17:45:08 +0530

    Added logic and styling for the react comment section

--- react-comment-section/src/App.jsx
+++ react-comment-section/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import Comment from "./components/Comment";
 import "./App.css";
+import { useNode } from "./hooks/useNode";
 
 const comments = {
   id: 1,
@@ -8,13 +10,33 @@ const comments = {
 
 function App() {
   // const [count, setCount] = useState(0);
+  const { insertNode, editNode, deleteNode } = useNode();
 
   const [commentsData, setCommentsData] = useState(comments);
+
+  //handler functions for adding, editing and deleting nodes
+  const handleInsertNode = (folderId, item) => {
+    const finalStructure = insertNode(commentsData, folderId, item);
+    setCommentsData(finalStructure);
+  };
+  const handleEditNode = (folderId, item) => {
+    const finalStructure = editNode(commentsData, folderId, item);
+    setCommentsData(finalStructure);
+  };
+  const handleDeleteNode = (folderId) => {
+    const finalStructure = deleteNode(commentsData, folderId);
+    setCommentsData({ ...finalStructure });
+  };
 
   return (
     <>
       <div className="App">
-        <Comment comments={commentsData} />
+        <Comment
+          comments={commentsData}
+          handleInsertNode={handleInsertNode}
+          handleEditNode={handleEditNode}
+          handleDeleteNode={handleDeleteNode}
+        />
       </div>
     </>
   );
--- react-comment-section/src/components/Comment.jsx
+++ react-comment-section/src/components/Comment.jsx
@@ -1,16 +1,59 @@
 import React from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import ButtonAction from "./ButtonAction";
+import "./Comment.css";
 
-const Comment = ({ comments }) => {
+const Comment = ({
+  comments,
+  handleInsertNode,
+  handleDeleteNode,
+  handleEditNode,
+}) => {
   const [inputValue, setInputValue] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState(false);
+
+  const inputRef = useRef(null);
 
   const changeInputHandler = (e) => {
     setInputValue(e.target.value);
   };
 
+  //for replying
+  const handleNewComment = () => {
+    setShowInput(true);
+    if (!inputValue) return;
+  };
+
   //for adding comments
-  const onAddComment = () => {};
+  const onAddComment = () => {
+    if (edit) {
+      if (inputRef?.current?.innerText === "") return;
+      handleEditNode(comments.id, inputRef?.current?.innerText);
+    } else {
+      if (!inputValue) return;
+      // setExpand(true);
+      handleInsertNode(comments.id, inputValue);
+      setInputValue("");
+      setShowInput(false);
+    }
+
+    if (edit) {
+      setEdit(false);
+    }
+  };
+
+  const handleDelete = () => {
+    handleDeleteNode(comments.id);
+  };
+
+  //for focusing text during edit
+  useEffect(() => {
+    inputRef?.current?.focus();
+  }, [edit]);
 
   return (
     <div>
@@ -25,6 +68,7 @@ const Comment = ({ comments }) => {
               value={inputValue}
               className="inputContainer__input first_input"
               autoFocus
+              placeholder="type something..."
             />
             <ButtonAction
               className="reply comment"
@@ -34,32 +78,100 @@ const Comment = ({ comments }) => {
           </>
         ) : (
           <>
-            <span style={{ paddingLeft: "25" }}>{comments.name}</span>
-            <div style={{ display: "flex", marginTop: "5px" }}>
-              <ButtonAction
-                className="reply comment"
-                type="Reply"
-                handleClick={onAddComment}
-              />
-              <ButtonAction
-                className="reply comment"
-                type="Reply"
-                handleClick={onAddComment}
-              />
-              <ButtonAction
-                className="reply comment"
-                type="Reply"
-                handleClick={onAddComment}
-              />
+            <span
+              ref={inputRef}
+              style={{
+                wordWrap: "break-word",
+              }}
+              contentEditable={edit}
+              suppressContentEditableWarning={edit}
+            >
+              {comments.name}
+            </span>
+
+            <div style={{ display: "flex", marginTop: "2px" }}>
+              {edit ? (
+                <div className="editmode_buttons">
+                  <ButtonAction
+                    type="Save"
+                    className="reply comment"
+                    handleClick={onAddComment}
+                  />
+                  <ButtonAction
+                    type="Cancel"
+                    className="reply comment"
+                    handleClick={() => {
+                      if (inputRef.current) {
+                        inputRef.current.innerText = comments.name;
+                      }
+                      setEdit(false);
+                    }}
+                  />
+                </div>
+              ) : (
+                <>
+                  <ButtonAction
+                    className="reply comment"
+                    type="Reply"
+                    handleClick={handleNewComment}
+                  />
+                  <ButtonAction
+                    className="edit comment"
+                    type="Edit"
+                    handleClick={() => setEdit(true)}
+                  />
+                  <ButtonAction
+                    className="delete comment"
+                    type="Delete"
+                    handleClick={handleDelete}
+                  />
+                </>
+              )}
             </div>
           </>
         )}
       </div>
 
+      {/* for showing input box when adding new comments */}
+      <div
+        style={{ display: showInput ? "block" : "none", paddingLeft: "25px" }}
+      >
+        {showInput && (
+          <div className="inputContainer">
+            <input
+              onChange={changeInputHandler}
+              type="text"
+              value={inputValue}
+              className="inputContainer__input first_input"
+              autoFocus
+              placeholder="type something..."
+            />
+            <ButtonAction
+              className="reply comment"
+              type="Reply"
+              handleClick={onAddComment}
+            />
+            <ButtonAction
+              className="reply comment"
+              type="Cancel"
+              handleClick={() => setShowInput(false)}
+            />
+          </div>
+        )}
+      </div>
+
       {/* recursively going through the comments */}
-      <div style={{ paddingLeft: "25" }}>
+      <div style={{ paddingLeft: "25px" }}>
         {comments?.items?.map((cmt, idx) => {
-          return <Comment key={cmt.id} comments={cmt} />;
+          return (
+            <Comment
+              key={cmt.id}
+              comments={cmt}
+              handleInsertNode={handleInsertNode}
+              handleEditNode={handleEditNode}
+              handleDeleteNode={handleDeleteNode}
+            />
+          );
         })}
       </div>
     </div>
