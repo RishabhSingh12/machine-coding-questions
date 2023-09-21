2175a2c7f39e11d4cdce6b6d0ab14eafa6e5c5c2
Rishabh Kumar <rishabh2807s@gmail.com>
2023-09-21 22:40:50 +0530

    Added selection logic , styling and logic for clearing selections

--- react-multiselect-dropdown/src/components/Select.tsx
+++ react-multiselect-dropdown/src/components/Select.tsx
@@ -1,4 +1,5 @@
 import styles from "./select.module.css";
+import { useState } from "react";
 
 type SelectOption = {
   value: any;
@@ -12,17 +13,58 @@ type SelectProps = {
 };
 
 export function Select({ value, onChange, options }: SelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openOptionHandler = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  //clearing options
+  const clearHandler = (e: any) => {
+    e.stopPropagation();
+    onChange(undefined);
+  };
+
+  //check for the current selected option
+  const isOptionSelected = (option: SelectOption) => {
+    return option === value;
+  };
+
+  //choosing a single option
+  const selectOption = (option: SelectOption) => {
+    onChange(option);
+  };
+
+  const selectOptionHandler = (e: any, option: SelectOption) => {
+    e.stopPropagation();
+    selectOption(option);
+    setIsOpen(false);
+  };
+
   return (
-    <div tabIndex={0} className={styles.container}>
-      <span className={styles.value}>Value</span>
-      <button className={styles["clear-btn"]}>&times;</button>
+    <div
+      onBlur={() => setIsOpen(false)}
+      onClick={openOptionHandler}
+      tabIndex={0}
+      className={styles.container}
+    >
+      <span className={styles.value}>{value?.label}</span>
+      <button onClick={clearHandler} className={styles["clear-btn"]}>
+        &times;
+      </button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
       {/* showing list of options */}
-      <ul className={`${styles.options} ${styles.show}`}>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
         {options.map((option) => {
           return (
-            <li key={option.label} className={styles.option}>
+            <li
+              onClick={(e) => selectOptionHandler(e, option)}
+              key={option.label}
+              className={`${styles.option} ${
+                isOptionSelected(option) ? styles.selected : ""
+              }`}
+            >
               {option.label}
             </li>
           );
