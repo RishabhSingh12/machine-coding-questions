c73605127d4beeee151c5280141b6ef980ff69bc
Rishabh Kumar <rishabh2807s@gmail.com>
2023-09-21 23:51:34 +0530

    added more logic for multi select and types for both single and multiple select along with necessary styling of remove btns

--- react-multiselect-dropdown/src/App.tsx
+++ react-multiselect-dropdown/src/App.tsx
@@ -11,15 +11,18 @@ const options = [
 ];
 
 function App() {
-  const [value, setValue] = useState<SelectOption[]>([options[0]]);
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
   return (
     <>
       <Select
         multiple
         options={options}
-        value={value}
-        onChange={(o) => setValue(o)}
+        value={value1}
+        onChange={(o) => setValue1(o)}
       />
+      <br />
+      <Select options={options} value={value2} onChange={(o) => setValue2(o)} />
     </>
   );
 }
