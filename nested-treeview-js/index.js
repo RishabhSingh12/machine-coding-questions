883082839a2d768437b8490bb9e97872ffe653ce
Rishabh Kumar <rishabh2807s@gmail.com>
2023-10-09 11:55:00 +0530

    Added the nested data for th nested structure

---
+++ nested-treeview-js/index.js
@@ -0,0 +1,27 @@
+let data = [
+  {
+    name: "furniture",
+    items: [
+      { name: "Tables & Chairs" },
+      {
+        name: "Sofas",
+        items: [
+          { name: "Bean Bag" },
+          { name: "Arm Chair" },
+          { name: "Modular" },
+        ],
+      },
+      { name: "Occassional" },
+    ],
+  },
+  {
+    name: "Decor",
+    items: [
+      { name: "Bed Linen" },
+      { name: "Curtains & Blinds" },
+      { name: "Carpets" },
+    ],
+  },
+];
+
+//render function
