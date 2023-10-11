079066db762b7c02cc800f37c64c1a32f2e823c4
Rishabh Kumar <rishabh2807s@gmail.com>
2023-10-11 17:31:51 +0530

    Added a search function for searching through the nested directory according to the user input

--- nested-treeview-js/index.js
+++ nested-treeview-js/index.js
@@ -1,6 +1,7 @@
 import { data } from "./data.js";
 
 const listContainer = document.getElementById("listContainer");
+const inputBox = document.getElementById("input");
 
 //for rendering nested lists
 function renderList(htmlStr, list) {
@@ -8,8 +9,8 @@ function renderList(htmlStr, list) {
     htmlStr += `<li>${item.name}</li>`;
 
     //if list contains more items
-    if (item.items) {
-      htmlStr += `<ul>${renderList(htmlStr, item.items)}</ul>`;
+    if (item.items && item.items.length > 0) {
+      htmlStr += `<ul>${renderList("", item.items)}</ul>`;
     }
   });
 
@@ -24,7 +25,39 @@ const renderData = (data) => {
 };
 
 //for searching the nested lists
-const searchNestedList = (list, keyword) => {};
+const searchNestedList = (list, keyword) => {
+  return list.reduce((acc, item) => {
+    //making a copy of each item
+    const newItem = { ...item };
+
+    //checking whether each item has items property as an array
+    if (Array.isArray(item.items)) {
+      newItem.items = searchNestedList(item.items, keyword);
+    }
+
+    //to check whether the current "name" property or the subsequent children items name matches ther user input
+    if (
+      newItem.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      (Array.isArray(newItem.items) && newItem.items.length > 0)
+    ) {
+      acc.push(newItem);
+    }
+
+    return acc;
+  }, []);
+};
 
 //calling the render function
 renderData(data);
+
+// search function
+function search(value) {
+  let filteredData = searchNestedList(data, value);
+  renderData(filteredData);
+}
+
+//calling search function
+inputBox.addEventListener("keyup", (e) => {
+  console.log("here");
+  search(e.target.value);
+});
