53d08d20ec62c1a3bca6bc772430aef27123541d
Rishabh Kumar <rishabh2807s@gmail.com>
2023-10-15 23:38:42 +0530

    Added logic for submission of form and generation of password

--- password-generator-js/index.js
+++ password-generator-js/index.js
@@ -1,10 +1,13 @@
 const characterAmountRange = document.getElementById("characterAmountRange");
 const characterAmountNumber = document.getElementById("characterAmountNumber");
 
+//getting user selections
+const includeUpperCaseElement = document.getElementById("includeUpperCase");
+const includeNumbersElement = document.getElementById("includeNumbers");
+const includeSymbolsElement = document.getElementById("includeSymbols");
+
 characterAmountNumber.addEventListener("input", syncCharacterAmount);
 characterAmountRange.addEventListener("input", syncCharacterAmount);
-
-const form = document.getElementById("passwordGeneratorForm");
 
 function syncCharacterAmount(e) {
   const value = e.target.value;
@@ -12,16 +15,31 @@ function syncCharacterAmount(e) {
   characterAmountRange.value = value;
 }
 
+const form = document.getElementById("passwordGeneratorForm");
 //submitting the form
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  //getting the user selected length for the password
   const characterAmount = characterAmountNumber.value;
+
+  //getting state of user selections
+  const includeUpperCase = includeUpperCaseElement.checked;
+  const includeNumbers = includeNumbersElement.checked;
+  const includeSymbols = includeSymbolsElement.checked;
 
   const password = generatePassword(
     characterAmount,
     includeUpperCase,
-    inckudeNumbers,
+    includeNumbers,
     includeSymbols
   );
 });
+
+//logic for generating password
+function generatePassword(
+  characterAmount,
+  includeUpperCase,
+  includeNumbers,
+  includeSymbols
+) {}
